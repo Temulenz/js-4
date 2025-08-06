f186421b117e264fa1925cbe88fd86560648649b
Temulenz <amitekik@gmail.com>
2025-08-06 09:15:48 +0800

    java

--- ungei site/unegui.js
+++ ungei site/unegui.js
@@ -1,1 +1,15 @@
+const body = document.createElement("body");
+const container = document.createElement("container");
+const img = document.createElement("img");
+const h1 = document.createElement("h1");
+const h2 = document.createElement("h2");
+const h3 = document.createElement("h3");
+const h4 = document.createElement("h4");
 
+body.appendChild(img);
+body.appendChild(h1);
+body.appendChild(h2);
+body.appendChild(h3);
+body.appendChild(h4);
+
+img.className;
