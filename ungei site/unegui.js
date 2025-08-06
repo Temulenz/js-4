faff045d64e92ac7ae375ffe252ae4e902ca51e1
Temulenz <amitekik@gmail.com>
2025-08-06 09:04:14 +0800

    s

--- ungei site/unegui.js
+++ ungei site/unegui.js
@@ -1,10 +1,1 @@
-// 1.find the last word
-// 2. return last worth length
-let text = "   fly me   to   the moon  ";
 
-const lengthOfLastWord = (s) => {
-  s.trim("").split(" ");
-  return arr[arr.length - 1].length;
-};
-
-console.log(s[i]);
