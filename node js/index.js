99fbcaf04bb1e651aa7501c82c68f4271e1437fc
Temulenz <amitekik@gmail.com>
2025-08-06 08:53:50 +0800

    s

---
+++ node js/index.js
@@ -0,0 +1,12 @@
+// 1.find the last word
+// 2. return last worth length
+let text = "   fly me   to   the moon  ";
+const lengthOfLastWord = (s) => {
+  for (let i = s.length - 1; i >= 0; i--) {
+    console.log(s[i]);
+
+    if (char !== " ") {
+      console.log(s[i]);
+    }
+  }
+};
---
+++ ungei site/unegui.js
@@ -0,0 +1,10 @@
+// 1.find the last word
+// 2. return last worth length
+let text = "   fly me   to   the moon  ";
+
+const lengthOfLastWord = (s) => {
+  s.trim("").split(" ");
+  return arr[arr.length - 1].length;
+};
+
+console.log(s[i]);
